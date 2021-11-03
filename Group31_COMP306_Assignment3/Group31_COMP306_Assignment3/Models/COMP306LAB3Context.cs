21d740ca05fef0bf4d31ed9abd6460ec72bee90e
Aleksandr Shelukheev <shelukheev@gmail.com>
2021-11-03 11:16:29 -0400

    added dbcontext

---
+++ Group31_COMP306_Assignment3/Group31_COMP306_Assignment3/Models/COMP306LAB3Context.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Group31_COMP306_Assignment3.Models
+{
+    public partial class COMP306LAB3Context : DbContext
+    {
+        public COMP306LAB3Context()
+        {
+        }
+
+        public COMP306LAB3Context(DbContextOptions<COMP306LAB3Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=mssql-lab3.ceps61n5ikx1.ca-central-1.rds.amazonaws.com;Initial Catalog=COMP306LAB3;User ID=user4lab3;Password=password;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Group31_COMP306_Assignment3/Group31_COMP306_Assignment3/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace Group31_COMP306_Assignment3.Models
+{
+    public partial class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Username ")]
+        [Display(Name = "UserName")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Password ")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
