26fef5b61d5f63acc97fcf8942e5e41f73d6ebb9
Videt <egor310501@mail.ru>
2021-11-03 07:38:55 -0400

    empty project

---
+++ Group31_COMP306_Assignment3/Group31_COMP306_Assignment3/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Group31_COMP306_Assignment3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
