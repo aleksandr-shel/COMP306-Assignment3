f08037826aa17218ae14efe6d40b88f7f4434bac
Videt <egor310501@mail.ru>
2021-11-03 08:09:53 -0400

    empty project

---
+++ Group31_COMP306_Assignment3/Group31_COMP306_Assignment3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Group31_COMP306_Assignment3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
